7bd08e897f0cf9a46d6da83f91430543944e1e00
Alok <Alok@AlokTalwar>
2016-04-08 11:03:11 -0700

    test code

---
+++ WebDriverDemoFeb/src/HomePage.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class HomePage {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.whiteboxqa.com/");
+		
+		
+		
+	}
+
+}
